b92459f2ff59a890155095ecd9e830b1131aeafb
Nicole Vargas <ariana.vargas.p@ucb.edu.bo>
2026-03-08 23:19:16 -0400

    frontend: Ventana de inicio

--- LibreriaJoelito/Program.cs
+++ LibreriaJoelito/Program.cs
@@ -9,18 +9,18 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
+// ESTO ES CLAVE PARA CSS, JS, IMÁGENES
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
