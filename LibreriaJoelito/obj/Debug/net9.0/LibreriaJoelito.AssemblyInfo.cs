0b36873645c9de15bedc834ecf96fd8e42b5c749
Zadropol <aaadrianrodriguezmontecinos@gmail.com>
2026-03-10 11:56:59 -0400

    merge con main y sub_main

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b88255fa7458933e9a1ebe112ea0e2cda13a81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9cbf1ec964b29fbf05522275fe249f30c0941a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
