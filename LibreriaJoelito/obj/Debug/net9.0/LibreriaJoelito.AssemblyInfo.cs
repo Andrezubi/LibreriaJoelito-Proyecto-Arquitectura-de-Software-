0e16c68d594bfe25f2fa43bf29f9e1218689d0cb
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2026-03-10 12:17:31 -0400

    commit para merge con main

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9272fec2bd699df060b78185e7abeb0370a14c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01140477bb0a7110daec88a6e03f2005c227219")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
