97f7890ed35498f4930b0deb5c98687e359492ed
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2026-03-10 03:06:56 -0400

    commit para cambiar de rama

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6f1cd8f8dc2341d672af18990e2d7e3cc923ec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e77d64330a2071ee480648f4953785fd39df0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
