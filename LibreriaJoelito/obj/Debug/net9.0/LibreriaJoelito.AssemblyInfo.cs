5335448f4029efa00ce89c0ed58e99587100f41a
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2026-03-10 14:46:26 -0400

    cambio de colores de botones

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39f8aea7202360b04b49b9ef94d1fe0d50d4019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1f264fdbdee3e52f365b4576a2ae303e8ed809")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
