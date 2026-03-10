adfb048e3ea32b47b967dfc2cdd9825a6b1e7d69
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2026-03-09 22:34:13 -0400

    arreglar model producto

--- LibreriaJoelito/Models/Producto.cs
+++ LibreriaJoelito/Models/Producto.cs
@@ -2,25 +2,36 @@
 {
     public class Producto
     {
-        public byte Id { get; set; }
+        public int Id { get; set; }
+
         public string Categoria { get; set; }
+
         public string Nombre { get; set; }
+
         public decimal Precio { get; set; }
+
         public int Stock { get; set; }
+
         public string TipoVenta { get; set; }
-        public decimal FactorConversion { get; set; }
-        public byte IdProductoBase { get; set; }
-        public byte Estado { get; set; }
+
+        public decimal? FactorConversion { get; set; }
+
+        public int? IdProductoBase { get; set; }
+
+        public bool Estado { get; set; }
+
         public DateTime FechaRegistro { get; set; }
-        public DateTime UltimaActualizacion { get; set; }
-        public byte IdUsuario { get; set; }
+
+        public DateTime? UltimaActualizacion { get; set; }
+
+        public int? IdUsuario { get; set; }
 
         public Producto()
         {
 
         }
 
-        public Producto(string categoria, string nombre, decimal precio, int stock, string tipoVenta, decimal factorConversion)
+        public Producto(string categoria, string nombre, decimal precio, int stock, string tipoVenta, decimal factorConversion,int? idProductoBase)
         {
             Categoria = categoria;
             Nombre = nombre;
@@ -28,6 +39,7 @@
             Stock = stock;
             TipoVenta = tipoVenta;
             FactorConversion = factorConversion;
+            IdProductoBase = idProductoBase;
         }
     }
 }
