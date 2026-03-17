6183a8fc3e857515e53b15637be4958658c899e5
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2026-03-16 22:56:50 -0400

    commit antes de cambio de rama

--- LibreriaJoelito/FactoryProducts/ProductoRepository.cs
+++ LibreriaJoelito/FactoryProducts/ProductoRepository.cs
@@ -1,6 +1,28 @@
-﻿namespace LibreriaJoelito.FactoryProducts
+﻿using LibreriaJoelito.Models;
+using System.Data;
+
+namespace LibreriaJoelito.FactoryProducts
 {
-    public class ProductoRepository
+    public class ProductoRepository : IRepository<Producto>
     {
+        public int Delete(Producto t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Producto t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Producto t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
