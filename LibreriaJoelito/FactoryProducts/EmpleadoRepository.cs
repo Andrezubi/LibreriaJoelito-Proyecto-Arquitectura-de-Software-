e96d0b17be7064eac377f93876598179ed024326
Camila <camila.carvajal@ucb.edu.bo>
2026-03-18 14:03:45 -0400

    refactor: create empleado adapted to factory method design pattern && fix: wrong validations in empleado validator

--- LibreriaJoelito/FactoryProducts/EmpleadoRepository.cs
+++ LibreriaJoelito/FactoryProducts/EmpleadoRepository.cs
@@ -10,7 +10,49 @@ namespace LibreriaJoelito.FactoryProducts
     {
         public int Insert(Empleado t)
         {
-            return 0;
+            string query = @"INSERT INTO Empleado (
+                                Nombre,
+                                ApellidoPaterno,
+                                ApellidoMaterno,
+                                Ci,
+                                Complemento,
+                                FechaNacimiento,
+                                Email,
+                                DireccionDomicilio,
+                                Telefono,
+                                FechaIngreso,
+                                Estado,
+                                FechaRegistro,
+                                FechaUltimaActualizacion,
+                                IdEmpleadoCambio) VALUES (
+                                @nombre,
+                                @apellidoPaterno,
+                                @apellidoMaterno,
+                                @ci,
+                                @extensionCi,
+                                @fechaNacimiento,
+                                @email,
+                                @direccionDomicilio,
+                                @telefono,
+                                @fechaIngreso,
+                                TRUE,
+                                CURRENT_TIMESTAMP,
+                                CURRENT_TIMESTAMP,
+                                NULL);";
+
+            MySqlCommand command = new MySqlCommand(query);
+            command.Parameters.AddWithValue("@nombre", t.Nombre);
+            command.Parameters.AddWithValue("@apellidoPaterno", t.ApellidoPaterno);
+            command.Parameters.AddWithValue("@apellidoMaterno", t.ApellidoMaterno);
+            command.Parameters.AddWithValue("@ci", t.Ci);
+            command.Parameters.AddWithValue("@extensionCi", t.Complemento ?? "");
+            command.Parameters.AddWithValue("@email", t.Email);
+            command.Parameters.AddWithValue("@direccionDomicilio", t.DireccionDomicilio);
+            command.Parameters.AddWithValue("@telefono", t.Telefono);
+            command.Parameters.AddWithValue("@fechaIngreso", t.FechaIngreso.ToString("yyyy-MM-dd"));
+            command.Parameters.AddWithValue("@fechaNacimiento", t.FechaNacimiento.ToString("yyyy-MM-dd"));
+
+            return RepositorioBD.ExecuteNonQuery(command);
         }
         public int Update(Empleado t)
         {
