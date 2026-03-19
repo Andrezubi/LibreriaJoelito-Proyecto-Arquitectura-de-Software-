4fb6dfb5cbaac5450dcf547f6f2e6b11e9899c79
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-03-19 09:01:23 -0400

    Merge completo con el main actual

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffdaadd825eac2a027b17229fd8c6fff7ea7065d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e3ffa7f19d8dbe14f163beeb916cf74c96119af")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
