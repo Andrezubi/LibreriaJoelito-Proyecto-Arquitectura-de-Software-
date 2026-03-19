7e3ffa7f19d8dbe14f163beeb916cf74c96119af
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-03-19 02:45:05 -0400

    merge Commit actual

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e912f76c29d41c90eec1b6dd694e3223d8e756bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffdaadd825eac2a027b17229fd8c6fff7ea7065d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
