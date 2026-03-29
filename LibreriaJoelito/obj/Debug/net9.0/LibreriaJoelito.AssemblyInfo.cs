ec4266a1ee0863316d3aa80eb1ad05a8623ca294
Nicole Vargas <ariana.vargas.p@ucb.edu.bo>
2026-03-29 02:09:49 -0400

    correccion de errores

--- LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
+++ LibreriaJoelito/obj/Debug/net9.0/LibreriaJoelito.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b752c903b396d33c29346d58ee9f363b8c121b9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6677858b557b7a87a2dde2ef0dbe0ca3598b64d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibreriaJoelito")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
