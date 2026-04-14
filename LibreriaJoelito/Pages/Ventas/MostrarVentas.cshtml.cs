334b714b27a3a8c7ed50abe76f85344aae79ec3c
Vanessa745 <vanessa.canaviri@ucb.edu.bo>
2026-04-14 17:09:25 -0400

    feat: página para mostrar ventas harcodeada

---
+++ LibreriaJoelito/Pages/Ventas/MostrarVentas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LibreriaJoelito.Pages.Ventas
+{
+    public class MostrarVentasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
