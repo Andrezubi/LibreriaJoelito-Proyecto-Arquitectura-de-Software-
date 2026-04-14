afd1480d62c484dfee6785d38edb68aa1a4ce0c8
Camila <camila.carvajal@ucb.edu.bo>
2026-04-13 23:42:22 -0400

    feat: model DetalleVenta.cs

---
+++ LibreriaJoelito/Dominio/Models/DetalleVenta.cs
@@ -0,0 +1,28 @@
+﻿namespace LibreriaJoelito.Dominio.Models
+{
+    #region Atributos
+    public class DetalleVenta
+    {
+        public int IdVenta { get; set; }
+        public int IdProducto { get; set; }
+
+        public int IdPresentacion { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        public decimal Subtotal => Cantidad * PrecioUnitario;
+    #endregion
+        #region Constructores
+        public DetalleVenta() { }
+        public DetalleVenta(int idVenta, int idProducto, int idPresentacion, int cantidad, decimal precioUnitario)
+        {
+            IdVenta = idVenta;
+            IdProducto = idProducto;
+            IdPresentacion = idPresentacion;
+            Cantidad = cantidad;
+            PrecioUnitario = precioUnitario;
+        }   
+        #endregion
+    }
+
+}
